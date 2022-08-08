02a8bfa99df6575b34eb5b72e708defbf35e0bd5
duatpwnd <duatpwnd@snaptag.co.kr>
2022-08-08 16:34:59 +0900

    add user menu of header

--- src/components/layout/header/TheHeader.vue
+++ src/components/layout/header/TheHeader.vue
@@ -8,7 +8,13 @@
   const routeName = computed(() => {
     return useRoute().name;
   });
+  const isActiveUserMenu = ref(false);
+  const activeMenu = ref(0);
   const breakPoint = ref("laptop+");
+  const closeMenu = (index: number) => {
+    isActiveUserMenu.value = false;
+    activeMenu.value = index;
+  };
   const activeNavigation = () => {
     if (breakPoint.value == "laptop+") {
       breakPoint.value = "";
@@ -19,7 +25,7 @@
 </script>
 <template>
   <header>
-    <mq-responsive target="tablet-">
+    <mq-responsive target="tablet-" tag="span">
       <svg-icon
         :fa-icon="faBars"
         class="menu-ico"
@@ -34,8 +40,23 @@
         title="logo"
       />
     </router-link>
-    <router-link to="/dashboard" class="dashboard-link">대시보드</router-link>
-    <router-link to="/help/faq" class="faq-link">문의하기</router-link>
+    <div class="right-menu">
+      <router-link to="/dashboard" class="dashboard-link">대시보드</router-link>
+      <router-link to="/help/faq" class="faq-link">문의하기</router-link>
+      <div class="user-wrap">
+        <span class="user-id" @click="isActiveUserMenu = !isActiveUserMenu"
+          >snaptag.co.kr</span
+        >
+        <ul class="user-menu" v-show="isActiveUserMenu">
+          <li :class="{ active: activeMenu == 0 }" @click="closeMenu(0)">
+            내정보
+          </li>
+          <li :class="{ active: activeMenu == 1 }" @click="closeMenu(1)">
+            로그아웃
+          </li>
+        </ul>
+      </div>
+    </div>
   </header>
   <mq-responsive :target="breakPoint">
     <Navigation v-if="routeName != 'signIn' && routeName != 'help'" />
@@ -51,23 +72,56 @@
     height: 80px;
     border-bottom: 1px solid #2f3137;
     background-color: #1a1b1e;
-    .dashboard-link,
-    .faq-link {
-      vertical-align: -3px;
-      color: #b8bfc4;
-      margin-right: 40px;
-    }
-    // .router-link-active {
-    //   color: #7091ff;
-    // }
-    .menu-ico {
-      color: #ccc;
-      vertical-align: middle;
+    .right-menu {
       position: absolute;
+      height: 21px;
       top: 0;
       right: 32px;
       bottom: 0;
       margin: auto;
+      .user-wrap {
+        display: inline-block;
+        position: relative;
+        .user-id {
+          display: inline-block;
+          padding-right: 20px;
+          background: url("@/assets/images/selectbox_arrow_bottom.svg")
+            no-repeat center right / 12px 12px;
+          color: #757b8a;
+        }
+        .user-menu {
+          position: absolute;
+          top: 36px;
+          right: 0;
+          width: 140px;
+          padding: 8px 16px;
+          box-sizing: border-box;
+          color: #525660;
+          background: #1a1b1e;
+          border: 1px solid #2f3137;
+          box-shadow: 4px 4px 8px rgba(0, 0, 0, 0.08);
+          border-radius: 4px;
+          li {
+            padding: 11px 0;
+          }
+          li.active {
+            color: #b7bac2;
+          }
+        }
+      }
+      .dashboard-link,
+      .faq-link {
+        color: #969ba6;
+        margin-right: 40px;
+      }
+      // .router-link-active {
+      //   color: #7091ff;
+      // }
+    }
+    .menu-ico {
+      color: #ccc;
+      vertical-align: middle;
+      margin-left: 32px;
     }
   }
 </style>
